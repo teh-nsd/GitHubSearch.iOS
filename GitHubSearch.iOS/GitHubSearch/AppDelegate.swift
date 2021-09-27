dd93bc42214ee516e4853b4116b4c5b1b7a185c1
Nikolay <nikolay.dimitrov@eldersoss.com>
2021-09-27 11:28:31 +0300

    Added AppDelegate

---
+++ GitHubSearch.iOS/GitHubSearch/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  GitHubSearch.iOS
+//
+//  Created by Nikolay Dimitrov on 27.09.21.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        print("Your code here")
+        return true
+    }
+}
