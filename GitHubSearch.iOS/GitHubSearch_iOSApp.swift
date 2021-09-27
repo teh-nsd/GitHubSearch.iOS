9086bda16d3a96150bd167c548db5ff2649d5898
Nikolay <nikolay.dimitrov@eldersoss.com>
2021-09-27 11:08:00 +0300

    Created the xcode project

---
+++ GitHubSearch.iOS/GitHubSearch_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearch_iOSApp.swift
+//  GitHubSearch.iOS
+//
+//  Created by Nikolay Dimitrov on 27.09.21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearch_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
